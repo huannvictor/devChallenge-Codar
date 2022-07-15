d606fed25b0c9a0c19fbc4bb43a5ff7234c947ae
Huann Almeida <huannvictor@gmail.com>
2022-07-15 00:46:34 -0300

    HTML & JS finished

---
+++ js/main.js
@@ -0,0 +1,22 @@
+import { testimonialsList } from "./testimonials.js";
+
+const testimonialsElement = document.getElementById("testimonial-container");
+
+testimonialsList.map((element, key) => {
+  let card = document.createElement("div");
+  let picture = document.createElement("img");
+  let testimonialHolder = document.createElement("div");
+  let testimonialMessage = document.createElement("p");
+  let personName = document.createElement("p");
+  let companyName = document.createElement("p");
+
+  picture.src = element.image;
+  testimonialMessage.innerHTML = element.message;
+  personName.innerHTML = element.name;
+  companyName.innerHTML = element.company;
+
+  testimonialHolder.append(testimonialMessage, personName, companyName);
+  card.append(picture, testimonialHolder);
+
+  testimonialsElement.append(card);
+});
---
+++ js/testimonials.js
@@ -0,0 +1,16 @@
+export const testimonialsList = [
+  {
+    name: "Pedro Ferreira",
+    company: "Construtora Minari",
+    image: "https://randomuser.me/api/portraits/men/64.jpg",
+    message:
+      "Já tive experiências com outras 4 empresas da área e a Codar é sem sombra de dúvidas superior em todos os aspectos.",
+  },
+  {
+    name: "Maria Rita",
+    company: "Escola Criança Feliz",
+    image: "https://randomuser.me/api/portraits/women/59.jpg",
+    message:
+      "Processos bem simples e criação de um site exatamente como a minha escola queria!",
+  },
+];
